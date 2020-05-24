d768ef06c05a2449631898b6fe349ebadbeaa777
atleastzero <megan.obryan52@gmail.com>
2020-05-24 02:28:27 -0700

    Styled search.

--- src/components/Utilities/Corkboard/Box.js
+++ src/components/Utilities/Corkboard/Box.js
@@ -4,8 +4,7 @@ import React from 'react'
 const styles = {
   // border: '1px dashed gray',
   // padding: '0.5rem 1rem',
-  paddingTop: "30px",
-  backgroundColor: "gray",
+  backgroundColor: "#eeeefd",
   cursor: 'move'
 }
 const Box = ({ content }) => {
